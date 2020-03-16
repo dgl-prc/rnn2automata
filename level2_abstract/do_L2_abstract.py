3bf9ae8a3a83d66ab23353f2fefa288228bb78e1
Guoliang <dgl-prc@zju.edu.cn>
2020-03-16 20:33:42 +0800

    move get_L1_data_path to read_seq.py

--- level2_abstract/do_L2_abstract.py
+++ level2_abstract/do_L2_abstract.py
@@ -1,24 +1,14 @@
 import sys
-
 sys.path.append("../")
-import os
 from level2_abstract.aalergia import *
 from level2_abstract.read_seq import *
 from utils.constant import *
-
-
-def get_L1_data_path(partition_type, model_type, data_type, data_source, k):
-    L1_data_path = getattr(AbstractData.Level1, partition_type.upper())
-    L1_data_path = getattr(getattr(L1_data_path, model_type.upper()), data_type.upper())
-    L1_data_path = get_path(os.path.join(L1_data_path, "k={}".format(k), "{}.txt".format(data_source)))
-    return L1_data_path
-
 
 def do_L2_abs():
     # k = int(sys.argv[1])
 
     using_train_set = False
-    partition_type = "hc"
+    partition_type = PartitionType.KM
     model_type = ModelType.LSTM
     data_type = DateSet.MR
     total_symbols = 500000
@@ -28,10 +18,10 @@ def do_L2_abs():
     output_folder = get_path(getattr(getattr(temp1, model_type.upper()), data_type.upper()))
     output_path = os.path.join(output_folder, data_source)
 
-    for k in range(8, 22, 2):
+    for k in range(2, 4, 2):
         print("***********k={}*********".format(k))
         data_path = get_L1_data_path(partition_type, model_type, data_type, data_source, k)
-        sequence, alphabet = load_data(data_path, total_symbols)
+        sequence, alphabet = load_trace_data(data_path, total_symbols)
         print(current_timestamp(), "init")
         al = AALERGIA(64, sequence, alphabet, start_symbol=START_SYMBOL, output_path=output_path, show_merge_info=False)
         print(current_timestamp(), "learing....")
