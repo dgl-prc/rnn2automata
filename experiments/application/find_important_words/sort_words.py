41558fc6ae5d8d49aa3643dac96fc5e4e592b22b
Guoliang <dgl-prc@zju.edu.cn>
2020-03-17 23:14:06 +0800

    add score funciton

--- experiments/application/find_important_words/sort_words.py
+++ experiments/application/find_important_words/sort_words.py
@@ -1,10 +1,26 @@
 import sys
+
 sys.path.append('../../../')
 from level1_abstract.clustering_based import *
 from level2_abstract.read_seq import *
 from level2_abstract.do_L2_abstract import get_L1_data_path
 from experiments.exp_utils import *
 from experiments.application.find_important_words.get_reachability_matrix import reachability_matrix
+
+
+def get_word_score(desired_y, reach_probs):
+    eps = 0.0000001  # to avoid divided by zero
+    if desired_y == "P":
+        if reach_probs[1] > reach_probs[0]:
+            return reach_probs[1] / (reach_probs[0] + eps)
+        else:
+            return -1
+    else:
+        assert desired_y == "N"
+        if reach_probs[0] > reach_probs[1]:
+            return reach_probs[0] / (reach_probs[1] + eps)
+        else:
+            return -1
 
 
 def sort_words_by_rm(sent, l1_trace, reach_matrix, trans_func):
@@ -19,31 +35,25 @@ def sort_words_by_rm(sent, l1_trace, reach_matrix, trans_func):
         rank_list: list, in which each element is word and all words are sorted by the oder of significance.
     '''
     # add start symbol S and the predict label [P|N]
-    assert len(l1_trace) - len(sent) == 2, print(len(l1_trace),len(sent))
+    assert len(l1_trace) - len(sent) == 2, print(len(l1_trace), len(sent))
     c_id = 1  # current state id
     y_predict = l1_trace[-1]
     score_list = []
     for sigma, word in zip(l1_trace[1:-1], sent):
         sigma = str(sigma)
         if sigma not in trans_func[c_id]:
-            continue
+            break
+            # continue
         else:
             next_id = trans_func[c_id][sigma]
             reach_probs = reach_matrix[next_id - 1]
-            if y_predict == "P":
-                if reach_probs[1] > reach_probs[0]:
-                    score_list.append(reach_probs[1])
-                else:
-                    score_list.append(-1)
-            else:
-                assert y_predict == "N"
-                if reach_probs[0] > reach_probs[1]:
-                    score_list.append(reach_probs[0])
-                else:
-                    score_list.append(-1)
+            score = get_word_score(y_predict, reach_probs)
+            score_list.append(score)
     sort_idxs = np.argsort(score_list)[::-1]
     sorted_words = [sent[idx] for idx in sort_idxs if score_list[idx] != -1]
-    return sorted_words
+    sorted_idx = [idx for idx in sort_idxs if score_list[idx] != -1]
+    score_list = [score_list[idx] for idx in sort_idxs if score_list[idx] != -1]
+    return sorted_words, sorted_idx, score_list
 
 
 def main1(**kwargs):
@@ -91,6 +101,7 @@ def main1(**kwargs):
     print(
         "Total {} samples wrongly predicted, {:.4f} of these failed to get ordered".format(num_error, cnt2 / num_error))
 
+
 if __name__ == '__main__':
     # _pm_file = "/home/dgl/project/learn_automata_rnn/data/level2_results/km/lstm/test/lstm_mr_k4_49056.pm"
     # rm = reachability_matrix(_pm_file, _num_prop)
@@ -105,4 +116,4 @@ if __name__ == '__main__':
     _pt_type = PartitionType.KM
 
     main1(m_tp=_model_type, d_tp=_data_type, d_s=_data_source, pt_tp=_pt_type, t_syms=_total_symbols, k=_k,
-         num_prop=_num_prop)
+          num_prop=_num_prop)
