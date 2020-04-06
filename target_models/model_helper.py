cd96df19e379708214fdee3bba17df97a8b0f564
Guoliang <dgl-prc@zju.edu.cn>
2020-04-06 22:13:42 +0800

    add get_model_file

--- target_models/model_helper.py
+++ target_models/model_helper.py
@@ -32,13 +32,34 @@ def sent2tensor(sent, input_dim, word2idx, wv_matrix, device):
     return seq
 
 
-def load_model(model_type, dataset, device):
+def load_model(model_type, dataset, device, load_model_path=STANDARD_PATH):
     params = getattr(train_args, "args_{}_{}".format(model_type, dataset))()
     params["rnn_type"] = model_type
     model = init_model(params=params)
 
-    model_path = get_path(getattr(getattr(TrainedModel, model_type.upper()), dataset.upper()))
+    if load_model_path == STANDARD_PATH:
+        model_path = get_path(getattr(getattr(TrainedModel, model_type.upper()), dataset.upper()))
+    else:
+        model_path = get_path(load_model_path)
     model.load_state_dict(torch.load(model_path))
     model = model.to(device)
     model.eval()
     return model
+
+
+def get_model_file(data_type, model_type):
+    if data_type == DateSet.MR:
+        if model_type == ModelType.LSTM:
+            # model_file = "train_acc-0.7892-test_acc-0.7798.pkl"
+            model_file = "train_acc-0.7988-test_acc-0.7746.pkl"
+        else:
+            assert model_type == ModelType.GRU
+            # model_file = "train_acc-0.7813-test_acc-0.7840.pkl"
+            model_file = "train_acc-0.8096-test_acc-0.7732.pkl"
+    else:
+        if model_type == ModelType.LSTM:
+            model_file = "train_acc-0.8807-test_acc-0.8435.pkl"
+        else:
+            assert model_type == ModelType.GRU
+            model_file = "train_acc-0.8836-test_acc-0.8424.pkl"
+    return model_file
