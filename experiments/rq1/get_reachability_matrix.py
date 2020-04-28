6df10eb791be3e37b4a6a9bbe600b3dc5c8a145e
Guoliang <dgl-prc@zju.edu.cn>
2020-04-28 19:55:39 +0800

    old predict:use PMC in some cases

---
+++ experiments/rq1/get_reachability_matrix.py
@@ -0,0 +1,87 @@
+import sys
+sys.path.append("../../../")
+import re
+import subprocess
+import shutil
+import numpy as np
+from utils.constant import *
+from utils.time_util import folder_timestamp
+
+
+def _prepare_prism_data(pm_file, num_prop):
+    ''' The task of this func is to 1) make every state the initial state.
+                                    2) change the label name.
+
+    total_states:
+    pm_file:
+    :return:
+    '''
+    ROOT_FOLDER = os.getcwd()
+    folder_id = folder_timestamp()
+    tmp_data_path = os.path.join(ROOT_FOLDER, folder_id)
+    os.makedirs(tmp_data_path)
+    with open(pm_file, "r") as fr:
+        raw_pm_lines = fr.readlines()
+    total_lines = len(raw_pm_lines)
+    label_begin = raw_pm_lines.index("endmodule\n") + 2
+    label_patter = re.compile(r".*\"(\w+)\".*")
+    for idx in range(label_begin, total_lines):
+        line = raw_pm_lines[idx]
+        new_label = "L" + label_patter.match(line).group(1)
+        raw_pm_lines[idx] = re.sub(r"\"(\w+)\"", '\"' + new_label + '\"', line)
+
+    ptn = re.compile(r".*\[1\.\.(\d+)\].*")
+    total_states = int(ptn.match(raw_pm_lines[3]).group(1))
+    for start_s in range(1, total_states + 1):
+        for prop_id in range(1, num_prop + 1):
+            file_name = "s{}_p{}.pm".format(start_s, prop_id)
+            raw_pm_lines[3] = "s:[1..{}] init {};\n".format(total_states, start_s)
+            with open(os.path.join(tmp_data_path, file_name), "w") as fw:
+                fw.writelines(raw_pm_lines)
+    print("Total states:{}".format(total_states))
+    # print("Model-check files saved in {}".format(tmp_data_path))
+    return total_states, tmp_data_path
+
+
+def _get_reachability_prob(prism_script, data_path, start_s, prop_id):
+    pm_file = os.path.join(data_path, "s{}_p{}.pm".format(start_s, prop_id))
+    property_file = get_path(PROPERTY_FILE)
+    prism_command = " ".join([prism_script, pm_file, property_file, "-prop", str(prop_id)])
+    output = subprocess.getoutput(prism_command)
+    output = output.split("\n")
+    rst = float(output[-2].split()[1])
+    return rst
+
+
+def _get_matrix(total_states, num_prop, tmp_prism_data_path):
+    matrix = []
+    for start_s in range(1, total_states + 1):
+        row = []
+        for pro_id in range(1, num_prop + 1):  # must be 1-index
+            ele = _get_reachability_prob(PRISM_SCRIPT, tmp_prism_data_path, start_s, pro_id)
+            row.append(ele)
+        matrix.append(row)
+    matrix = np.array(matrix)
+    return matrix
+
+
+def reachability_matrix(pm_file, num_prop):
+    ''' calculate the matrix of label reachability.
+
+    Parameters
+    -------------------
+    pm_file: str. The path of original pm model file
+    num_prop: int. Number of property which need to satisfy.
+    Return: ndarray. shape(num_states, num_prop)
+    '''
+    ##################################
+    # prepare the pair of input files
+    ##################################
+    num_states, tmp_data_path = _prepare_prism_data(pm_file, num_prop)
+    matrix = _get_matrix(num_states, num_prop, tmp_data_path)
+    ##################################
+    # remove the prepared files
+    ##################################
+    shutil.rmtree(tmp_data_path)
+    # print("Removed temp files in {}".format(tmp_data_path))
+    return matrix
--- experiments/rq1/pfa_predict.py
+++ experiments/rq1/pfa_predict.py
@@ -2,19 +2,24 @@
 predict with the L1 trace.
 """
 import sys
+import shutil
+import time
 
-sys.path.append("../../../")
+sys.path.append("../../")
 from target_models.model_helper import get_model_file, get_input_dim
 from experiments.rq4.adv_detect.detect_utils import *
 from data.text_utils import is_use_clean
+from experiments.rq1.get_reachability_matrix import prepare_prism_data, get_state_reachability
 
 
-def test_acc_fdlt(test_X, test_Y, model, partitioner, dfa, word2idx, wv_matrix, use_clean=True, input_dim=300,
+def test_acc_fdlt(test_X, test_Y, model, partitioner, dfa, tmp_prims_data, word2idx, wv_matrix, use_clean=True,
+                  input_dim=300,
                   device="cpu", pt_type=PartitionType.KM):
     trans_func, trans_wfunc = dict(dfa["trans_func"]), dict(dfa["trans_wfunc"])
     acc = 0
     fdlt = 0
     unspecified = 0
+    reachab_chache = {}
     for x, y in zip(test_X, test_Y):
         if use_clean:
             x = filter_stop_words(x)
@@ -26,11 +31,17 @@ def test_acc_fdlt(test_X, test_Y, model, partitioner, dfa, word2idx, wv_matrix, 
                                    partitioner_exists=True, partition_type=pt_type)[0]
         _, L2_trace = get_path_prob(L1_trace, trans_func, trans_wfunc)
         last_inner = L2_trace[-2]
-        pprob = trans_wfunc[last_inner]["P"] if "P" in trans_wfunc[last_inner] else 0.
-        nprob = trans_wfunc[last_inner]["N"] if "N" in trans_wfunc[last_inner] else 0.
-
-        pfa_pred = np.argmax([nprob, pprob])
-
+        pprob = trans_wfunc[last_inner]["P"] if "P" in trans_wfunc[last_inner] else 0
+        nprob = trans_wfunc[last_inner]["N"] if "N" in trans_wfunc[last_inner] else 0
+        if (pprob + nprob) == 0:
+            if last_inner in reachab_chache:
+                probs = reachab_chache[last_inner]
+            else:
+                probs = get_state_reachability(tmp_prims_data, num_prop=2, start_s=last_inner)
+                reachab_chache[last_inner] = probs
+            pfa_pred = np.argmax(probs)
+        else:
+            pfa_pred = np.argmax([nprob, pprob])
         if pfa_pred == y:
             acc += 1
         if pfa_pred == rnn_pred:
@@ -54,10 +65,13 @@ if __name__ == '__main__':
     # _total_symbols = 54044
     ##############################
 
-    for _data_type in [DateSet.Tomita1, DateSet.Tomita2, DateSet.Tomita3, DateSet.Tomita4, DateSet.Tomita5,
-                       DateSet.Tomita6]:
-        for _model_type in [ModelType.LSTM, ModelType.GRU]:
-            print(_data_type.upper(),_model_type.upper())
+    # for _data_type in [DateSet.Tomita6, DateSet.Tomita7]:
+    #     for _model_type in [ModelType.GRU, ModelType.LSTM]:
+    test_on = "test_{}"
+    for _data_type in [DateSet.Tomita7]:
+        for _model_type in [ModelType.GRU]:
+            # for _model_type in [ModelType.GRU, ModelType.LSTM]:
+            print(_data_type.upper(), _model_type.upper())
             _total_symbols = get_total_symbols(_data_type)
             _device = "cpu"
             _pt_type = PartitionType.KM
@@ -81,15 +95,30 @@ if __name__ == '__main__':
                 data = load_pickle(get_path(getattr(DataPath, _data_type.upper()).PROCESSED_DATA))
                 wv_matrix = load_pickle(get_path(getattr(DataPath, _data_type.upper()).WV_MATRIX))
 
-            for _k in range(2, 22, 2):
-                pt_path = AbstractData.Level1.NO_STOPW.format(_data_type, _model_type, _k, _data_source + "_partition.pkl")
-                _dfa_file_path = AbstractData.Level2.NO_STOPW.format(_data_type, _model_type, _k, alpha)
-                _dfa_file_path = os.path.join(_dfa_file_path, "{}_{}_transfunc.pkl").format(_data_source, _total_symbols)
+            # for _k in range(61, 66, 1):
+            for _k in [64]:
+                pt_path = AbstractData.Level1.NO_STOPW.format(_data_type, _model_type, _k,
+                                                              _data_source + "_partition.pkl")
+                dfa_file_path = AbstractData.Level2.NO_STOPW.format(_data_type, _model_type, _k, alpha)
+                trans_func_file = os.path.join(dfa_file_path, "{}_{}_transfunc.pkl").format(_data_source,
+                                                                                            _total_symbols)
+                pm_file_path = os.path.join(dfa_file_path, "{}_{}.pm").format(_data_source,
+                                                                              _total_symbols)
                 partitioner = load_pickle(pt_path)
-                dfa = load_pickle(get_path(_dfa_file_path))
-                acc, fdlt, unspecified = test_acc_fdlt(data["test_x"], data["test_y"], model, partitioner, dfa,
+                dfa = load_pickle(get_path(trans_func_file))
+                # make reachability matrix
+                total_states, tmp_prims_data = prepare_prism_data(pm_file_path, num_prop=2)
+                if total_states > 100:
+                    shutil.rmtree(tmp_prims_data)
+                    break
+                acc, fdlt, unspecified = test_acc_fdlt(data[test_on.format('x')], data[test_on.format('y')], model,
+                                                       partitioner, dfa,
+                                                       tmp_prims_data,
                                                        data["word_to_idx"],
                                                        wv_matrix, use_clean=_use_clean, input_dim=input_dim,
                                                        device=_device, pt_type=_pt_type)
                 print(
-                    "k={}\tacc={:.4f}\tfdlt={:.4f}\tunspecified:{}/{}".format(_k, acc, fdlt, unspecified, len(data["test_y"])))
+                    "k={}\t#states={}\tacc={:.4f}\tfdlt={:.4f}\tunspecified:{}/{}".format(_k, total_states, acc, fdlt,
+                                                                                          unspecified,
+                                                                                          len(data["test_y"])))
+                shutil.rmtree(tmp_prims_data)
